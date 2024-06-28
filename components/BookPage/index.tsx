f935b9353482a3eff7c41dfd8ef163dba7fb8722
민우석 <s23044@gsm.hs.kr>
2024-06-29 01:28:53 +0900

    add Book Logic

--- components/BookPage/index.tsx
+++ components/BookPage/index.tsx
@@ -3,25 +3,47 @@ import * as S from './style';
 import { useEffect, useState } from 'react';
 import RecommandItem from './RecommandItem';
 import ApplicantModal from './ApplicantModal';
+import { RecommendBookType } from '@/types';
+import { instance } from '@/apis';
 
 const BookPage = () => {
-  const [selected, setSelected] = useState(true);
-  const [modal, setModal] = useState(false);
+  const [selected, setSelected] = useState<boolean>(true);
+  const [modal, setModal] = useState<boolean>(false);
+  const [type, setType] = useState<string>('NOVEL');
+  const [book, setBook] = useState<RecommendBookType[]>([]);
+  
+
+  const fetchBook = async () => {
+    try {
+      const { data } = await instance.get(`/recommend`, {
+        params: {
+          type: type,
+        },
+      });
+      setBook(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const changeType = () => {
+    setSelected(!selected);
+    setType(selected ? 'ESSAY' : 'NOVEL');
+  };
+
+  useEffect(() => {
+    fetchBook();
+  }, [type]);
+
   return (
     <>
       <S.Wrapper>
         <S.Header>
           <S.SelectionContainer>
-            <S.SelectedTitle
-              onClick={() => setSelected(!selected)}
-              selected={!!selected}
-            >
+            <S.SelectedTitle onClick={() => changeType()} selected={!!selected}>
               Novel
             </S.SelectedTitle>
-            <S.SelectedTitle
-              onClick={() => setSelected(!selected)}
-              selected={!selected}
-            >
+            <S.SelectedTitle onClick={() => changeType()} selected={!selected}>
               Essay
             </S.SelectedTitle>
           </S.SelectionContainer>
@@ -30,16 +52,15 @@ const BookPage = () => {
           </S.PlusContainer>
         </S.Header>
         <S.RecommandList>
-          <RecommandItem />
-          <RecommandItem />
-          <RecommandItem />
-          <RecommandItem />
-          <RecommandItem />
-          <RecommandItem />
-          <RecommandItem />
-          <RecommandItem />
-          <RecommandItem />
-          <RecommandItem />
+          {book &&
+            book.map((item) => (
+              <RecommandItem
+                key={item.id}
+                title={item.title}
+                author={item.author}
+                content={item.content}
+              />
+            ))}
         </S.RecommandList>
       </S.Wrapper>
       {modal && <ApplicantModal onClose={() => setModal(false)} />}
